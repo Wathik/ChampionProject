3223e07d94378e79a2c00b8180b2a43f857172d9
Wathik Almayali <walmayali@gmail.com>
2014-07-24 17:54:44 -0400

    first commit

---
+++ CommonViewController.h
@@ -0,0 +1,20 @@
+//
+//  CommonViewController.h
+//  ChampionProject
+//
+//  Created by Wathik Almayali on 7/24/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextView *CommonMonadyStroy;
+@property (strong, nonatomic) IBOutlet UITextView *CommonTuesdayStroy;
+@property (strong, nonatomic) IBOutlet UITextView *CommonWednesdayStory;
+@property (strong, nonatomic) IBOutlet UITextView *CommonThoursdayStory;
+@property (strong, nonatomic) IBOutlet UITextView *CommonFridayStory;
+@property (strong, nonatomic) IBOutlet UITextView *CommonSaturdayStory;
+@property (strong, nonatomic) IBOutlet UITextView *CommonSundayStory;
+
+@end
