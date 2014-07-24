80104625d86d64f770c55b95575a885192ade280
Wathik Almayali <walmayali@gmail.com>
2014-07-24 13:23:38 -0400

    Initial Commit

---
+++ ChampionProject/SecureViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecureViewController.h
+//  ChampionProject
+//
+//  Created by Wathik Almayali on 7/24/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecureViewController : UIViewController
+
+@end
